12bdaa9c65164adfeebc1dce70695c670a3d61b3
AngelTomasM <118596396+AngelTomasM@users.noreply.github.com>
2023-12-12 12:20:03 +0100

    Commit inicial

---
+++ Hagamos_un_Fork/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Hagamos_un_Fork
+{
+    partial class Hagamos_un_Fork
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnAccion = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(24, 31);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(473, 71);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "¡Hagamos un Fork!";
+            // 
+            // btnAccion
+            // 
+            this.btnAccion.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnAccion.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAccion.Location = new System.Drawing.Point(171, 105);
+            this.btnAccion.Name = "btnAccion";
+            this.btnAccion.Size = new System.Drawing.Size(144, 42);
+            this.btnAccion.TabIndex = 1;
+            this.btnAccion.Text = "Acción";
+            this.btnAccion.UseVisualStyleBackColor = false;
+            this.btnAccion.Click += new System.EventHandler(this.btnAccion_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(27, 271);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(41, 39);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "...";
+            // 
+            // Hagamos_un_Fork
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 39F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(643, 410);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnAccion);
+            this.Controls.Add(this.lblTitulo);
+            this.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Hagamos_un_Fork";
+            this.Text = "¡Hagamos un Fork!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnAccion;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Hagamos_un_Fork/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hagamos_un_Fork
+{
+    public partial class Hagamos_un_Fork : Form
+    {
+        public Hagamos_un_Fork()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccion_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = "Estoy esperando el merge de quien haga un fork";
+        }
+    }
+}
